f0151d0142e0ea4ebb575f9e4dd5a072a340c91f
FruegaGit <fruedagit@gmail.com>
2022-06-08 16:09:31 +0200

    initial commit

---
+++ Ejercicio 1/main.js
@@ -0,0 +1,31 @@
+console.log('Hola');
+
+var variable1 = 'variable1';
+let variable2 = 'variable2';
+const variable3 = 'variable3';
+
+console.log(typeof(variable1));
+
+var variableA = 1, variableB = 2;
+
+console.log(variableA+variableB);
+
+const objeto = {
+    name: 'Nombre',
+    apellido: 'Apellido'
+}
+
+console.log(objeto);
+console.table(objeto);
+
+const array = [1, true, "string"];
+console.log(array);
+console.log(array[0]);
+
+function saludar(){
+    console.log("hola");
+}
+
+saludar();
+
+
