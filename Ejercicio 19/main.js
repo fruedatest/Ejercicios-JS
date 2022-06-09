67cd1116cb02e4837fe5aa545a302fc6e29057e1
FruegaGit <fruedagit@gmail.com>
2022-06-09 13:38:06 +0200

    19commit

---
+++ Ejercicio 19/main.js
@@ -0,0 +1,8 @@
+
+document.getElementById("main-container").onmouseover = function (){
+    this.style.background = "url(cartaBack.JPG) no-repeat";
+}
+
+document.getElementById("main-container").onmouseout = function(){
+    this.style.background = "url(cartaFront.JPG) no-repeat";
+}
