fe6da2a49f207ac63eab7bf31c54b6267d177dd6
FruegaGit <fruedagit@gmail.com>
2022-06-09 10:41:20 +0200

    12commit

---
+++ Ejercicio 12/main.js
@@ -0,0 +1,16 @@
+const array = ['a', 'b', 'c', 'd', 'e'];
+
+array.forEach(element =>  {
+    console.log(element);
+});
+
+
+
+let numero = 5;
+
+while (numero >= 0) {
+  console.log(numero);
+  numero--;
+
+}
+
